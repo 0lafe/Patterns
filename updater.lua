94c87f02b1ab4df9a7c1b4bf7112aaaba9b1038d
0lafe <0lafethehorsescrub@gmail.com>
2021-12-17 02:02:11 -0500

    added updater

--- updater.lua
+++ updater.lua
@@ -2,7 +2,7 @@
 
 -- os.execute("wget https://raw.githubusercontent.com/0lafe/Patterns/main/main.lua -f home/Patterns/main")
 
-names = {"main", "oldMethod", "test", "dl"}
+names = {"main", "oldMethod", "test", "updater"}
 
 for i,name in ipairs(names) do 
     os.execute("wget https://raw.githubusercontent.com/0lafe/Patterns/main/"..name..".lua -f /home/Patterns/"..name..".lua") 
