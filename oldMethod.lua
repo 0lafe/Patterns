c2a063ea722528f608eae8325af6395cf8131102
0lafe <0lafethehorsescrub@gmail.com>
2021-12-16 22:19:51 -0500

    starting downloader

---
+++ oldMethod.lua
@@ -0,0 +1,29 @@
+local component = require "component"
+local iface = component.me_interface
+
+local guid = nil 
+for k, v in component.list() do 
+  if v == 'database' then 
+    guid = k
+    break
+  end
+end
+
+local d = component.proxy(guid) or error("database not found")
+local database = d.address
+
+i = 0
+while (i<64)
+do
+    i = i + 1
+    iface.setInterfacePatternInput(1, database, 1, 64, i)
+end
+
+i = 0
+while (i<128)
+do
+    i = i + 1
+    iface.setInterfacePatternOutput(1, database, 2, 64, i)
+end
+
+print("Done")
