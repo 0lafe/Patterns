5f3964c4ae67a1848f4ca8ba7e92c9d99bbff832
0lafe <0lafethehorsescrub@gmail.com>
2021-12-17 03:45:40 -0500

    reader in package manager

--- updater.lua
+++ updater.lua
@@ -1,4 +1,4 @@
-names = {"main", "oldMethod", "updater", "newMethod"}
+names = {"main", "oldMethod", "updater", "newMethod", "reader"}
 
 for i,name in ipairs(names) do 
     os.execute("wget https://raw.githubusercontent.com/0lafe/Patterns/main/"..name..".lua -f /home/Patterns/"..name..".lua") 
