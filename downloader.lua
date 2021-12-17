5a3814a8e7dcee08f9faf2c6bb2244bc686cfb92
0lafe <0lafethehorsescrub@gmail.com>
2021-12-17 01:27:24 -0500

    first test

--- downloader.lua
+++ downloader.lua
@@ -2,3 +2,4 @@
 
 os.execute("wget https://raw.githubusercontent.com/0lafe/Patterns/main/main.lua -f home/Patterns/main")
 
+names = ["main", "oldMethod", "test"]
--- main.lua
+++ main.lua
@@ -1,1 +1,2 @@
-print("Hello")
+print("Hello :p")
+print("time to do some coding")
