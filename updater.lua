01076882ee8073f0c8b56eb0c388b15db2b96905
0lafe <0lafethehorsescrub@gmail.com>
2021-12-17 02:25:57 -0500

    added new method to package manager

--- updater.lua
+++ updater.lua
@@ -1,4 +1,4 @@
-names = {"main", "oldMethod", "test", "updater"}
+names = {"main", "oldMethod", "test", "updater", "newMethod"}
 
 for i,name in ipairs(names) do 
     os.execute("wget https://raw.githubusercontent.com/0lafe/Patterns/main/"..name..".lua -f /home/Patterns/"..name..".lua") 
